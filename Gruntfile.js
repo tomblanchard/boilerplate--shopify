ff09542b2fbfe81ab707ba6f76a1f4977d27a29d
Tom Blanchard <yo@tomblanchard.co.uk>
2014-12-31 18:19:53 +0000

    Stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
           cwd: 'src/scss/framework',
           src: ['**/*.scss'],
           dest: 'shop/assets',
-          ext: '.css'
+          ext: '.min.css'
         }]
       }
     },
@@ -44,41 +44,11 @@ module.exports = function(grunt) {
     },
 
 
-    cssmin: {
-      dist: {
-        options : {
-          advanced: false,
-          aggressiveMerging: false,
-          keepBreaks: true,
-          keepSpecialComments: '*',
-          rebase: false,
-          shorthandCompacting: false
-        },
-        files: [{
-          expand: true,
-          cwd: 'shop/assets',
-          src: ['*.css'],
-          dest: 'shop/assets'
-        }]
-      }
-    },
-
-
-    cssbeautifier : {
-      files : ['shop/assets/*.css'],
-      options : {
-        indent: '  ',
-        openbrace: 'end-of-line',
-        autosemicolon: true
-      }
-    },
-
-
     shopify: {
       options: {
-        api_key: '76bf4dc1f4cbdebba23ee7c83ddd65aa',
-        password: 'eb35c842d5b2b1d306db0336642e382e',
-        url: 'playground-20.myshopify.com',
+        api_key: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        password: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        url: 'XXXXXXXXXX.myshopify.com',
         base: 'shop/'
       }
     },
@@ -87,15 +57,12 @@ module.exports = function(grunt) {
     watch: {
       sass: {
         files: 'src/scss/**/*.scss',
-        tasks: ['sass:dist', 'autoprefixer:dist', 'cssmin:dist', 'cssbeautifier']
+        tasks: ['sass:dist', 'autoprefixer:dist']
       },
 
       shopify: {
         files: ['shop/**/*'],
-        tasks: ['shopify'],
-        options: {
-          interval: 1000
-        },
+        tasks: ['shopify']
       }
     }
 
@@ -105,8 +72,6 @@ module.exports = function(grunt) {
   grunt.registerTask('default', [
     'sass:dist',
     'autoprefixer:dist',
-    'cssmin:dist',
-    'cssbeautifier',
     'shopify:upload',
     'watch'
   ]);
